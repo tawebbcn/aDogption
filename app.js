c6c3f8f223a902f890b67f0754c8ffe01024e042
Rapha Montenegro <raphaelmmontenegro@gmail.com>
2018-04-23 11:57:50 +0200

    set up html, css and /auth route

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
 'use strict';
+
+// -----------------Routes------------ //
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+
+// --------------------Set up routes--------------//
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
-// view engine setup
+// ---------------view engine setup------------ //
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/spots', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+// OR when using provess.env to get ready for deploy
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI, {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+// -------------------Middlewares--------------- //
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// ---------------Call the routes------------ //
 
-/// catch 404 and forward to error handler
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
+
+// ---------------Catch 404 and forward to error handler------------ //
 
 // NOTE: requires a views/not-found template
 app.use((req, res, next) => {
