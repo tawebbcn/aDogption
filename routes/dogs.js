782c5c2a20d05e51a588b6d5a333ff363d5319a8
Jorge Terol <jorgetd92@gmail.com>
2018-04-24 18:23:18 +0200

    Added dogs page

--- routes/dogs.js
+++ routes/dogs.js
@@ -5,4 +5,86 @@ const Dog = require('../models/dogs-model');
 const router = express.Router();
 const mongoose = require('mongoose');
 
+router.get('/', (req, res, next) => {
+  Dog.find({})
+    .then((result) => {
+      const data = {
+        dogs: result
+      };
+      res.render('dogs', data);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(404);
+    res.render('not-found');
+    return;
+  }
+  const dogId = req.params.id;
+  const user = req.session.user;
+  let shelter = false;
+
+  if (user.role === 'shelter') {
+    shelter = true;
+  }
+
+  Dog.findById(dogId)
+    .then((result) => {
+      if (!result) {
+        res.status(404);
+        res.render('not-found');
+        return;
+      }
+      const data = {
+        dogs: result,
+        user: user,
+        shelter: shelter
+      };
+      res.render('dog', data);
+    })
+    .catch(next);
+});
+
+router.get('/:id/request', (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(404);
+    res.render('not-found');
+    return;
+  }
+  const dogId = req.params.id;
+  const user = req.session.user;
+
+  Dog.findById(dogId)
+    .then((result) => {
+      if (!result) {
+        res.status(404);
+        res.render('not-found');
+        return;
+      }
+      const data = {
+        dogs: result,
+        user: user
+      };
+      res.render('request', data);
+    })
+    .catch(next);
+});
+
+// router.post('/:id/request', (req, res, next) => {
+//   const dogId = req.params.id;
+//   const userId = req.session.user._id;
+//   const message = req.body.message;
+
+//   let object = {
+//     owner: userId,
+//     message: message
+//   };
+
+//   Dog.findOneAndUpdate(dogId)
+//     .then((result) => {
+//       { push: { object: object; } }
+//     });
+// });
+
 module.exports = router;
